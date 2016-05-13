25c1da972348e9d8a43e153360096f65cc3f1b1e
Elia Bardashevich <ebardashevich@gmail.com>
2016-05-13 13:55:24 +0100

    api

--- src/interface.js
+++ src/interface.js
@@ -34,4 +34,19 @@ function updateTemperature() {
   $('#temperature').text(thermostat.temperature);
   $('#temperature').attr('class', thermostat.energyUsage());
 }
+
+
+$('#get-weather').click(function(){
+  var apiAddress = 'http://api.openweathermap.org/data/2.5/weather?units=metric';
+  var location = $('#weather-location').val();
+  var locationQuery = '&q=' + location;
+  var apiKey = '&appid=4e70879c6ea4f476773c1d673f688f3d';
+  $.get(apiAddress+locationQuery+apiKey, function(data){
+      $('#weather').text(Math.round(data.main.temp));
+  });
+});
+
+
+
+
 });
